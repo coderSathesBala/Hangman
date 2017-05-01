b0d56905b6b61acdc90a574afb41062cb101fd1f
Sathes Balasingam <sathes352@gmail.com>
2017-05-01 01:10:02 -0400

    last commit, last commit, last commit

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-var empty = [];
-var basketballTeams = ['TorontoRaptors', 'ChicagoBulls', 'DallasMavericks', 'BostonCeltics', 'ClevelandCavaliers', 'LosAngelesLakers', 'UtahJazz', 'GoldenStateWarriors', 'HoustonRockets', 'CharlotteBobcats', 'IndianaPacers']
+var empty
+var basketballTeams = ['Toronto   Raptors', 'Chicago   Bulls', 'Dallas   Mavericks', 'Boston   Celtics', 'Cleveland   Cavaliers', 'Los   Angeles   Lakers',
+                       'Utah   Jazz', 'Golden   State   Warriors', 'Houston   Rockets', 'Charlotte   Hornets', 'Indiana   Pacers', 'Atlanta   Hawks',
+                       'Brooklyn   Nets', 'Denver   Nuggets', 'Detroit   Pistons', 'Los   Angeles   Clippers', 'Memphis   Grizzlies', 'Milwaukee   Bucks',
+                       'Minnesota   Timberwolves', 'New   Orleans   Pelicans', 'New   York   Knicks', 'Oklahoma   City   Thunder', 'Orlando   Magic', 'Philadelphia   76ers',
+                       'Phoenix   Suns', 'Portland   Trail   Blazers', 'Sacramento   Kings', 'San   Antonio   Spurs', 'Washington  Wizards']
 var random
 var randomLetters
 var lettersLength
@@ -13,24 +17,35 @@ window.onload = function() {
 
 $(function newGame() {
   $('.newGame').on('click', function() {
+    empty = [];
     $('.title').html('HANGMAN BASKETBALL TEAMS')
     $('button').removeAttr('disabled', 'disabled')
     $('.photo').css('display', 'none')
     $('#picture1').css('display', 'inline-block')
-    random = basketballTeams[Math.floor(Math.random() * 10)];
+    random = basketballTeams[Math.floor(Math.random() * 29)];
     mistakes = 1;
     randomLetters = random.split('')
     lettersLength = randomLetters.length
     randomLength = random.length
     document.getElementById(this.id.id)
-  $('#letters').html('');
+  $('.letters').html('');
     console.log(random)
     var counter = 0
       for (i=0; i < randomLength; i++) {
-        $('#letters').append('<div class="underlined"><h1 class = individualLetters' + randomLetters[i].toLowerCase() + '>' + randomLetters[0+i] + '</h1></div>')
+        if (randomLetters[i] !== ' ') {
+        $('.letters').append('<div class="underlined"><h1 class = individualLetters' + randomLetters[i].toLowerCase() + '>' + randomLetters[0+i] + '</h1></div>')
             $('.individualLetters' + randomLetters[i].toLowerCase()).css({"padding-left" : "10px",
                                                                           "visibility"   : "hidden",
                                                                           "display"      : "inline-block"})
+      } else if (randomLetters[i] === ' ') {
+        empty.push(randomLetters[i])
+          $('.letters').append('<h1 class = individualLetters> a </h1>')
+          $('.individualLetters').css({"padding-left" : "10px",
+                                      "visibility"   : "hidden",
+                                      "display"      : "inline-block"})
+        } else {
+          console.log('why')
+        }
   };
     var counter = 0;
       for (i=0; i < randomLength - 1; i++) {
@@ -49,7 +64,7 @@ $(function chooseLetter() {
     var found = false;
       for (i=0; i < lettersLength; i++) {
         var letterCapital = (randomLetters[i].toUpperCase());
-          if (buttonLetter === letterCapital) {
+        if (buttonLetter === letterCapital) {
             empty.push(randomLetters[i])
             console.log(empty)
             found = true
